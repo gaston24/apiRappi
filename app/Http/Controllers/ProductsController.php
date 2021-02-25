c94c6e248210504759593360abb7a333e3e69bb8
Gaston <gaston.marcilio@gmail.com>
2021-02-25 16:36:25 -0300

    comienzo
    array de articulos

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -3,8 +3,43 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
 
 class ProductsController extends Controller
 {
-    //
+    
+    public function articulosNuevos(){
+
+        $articulosNuevos = DB::select("SELECT COD_ARTICU, DESCRIPCIO, DESC_ADIC FROM STA11 
+        WHERE COD_ARTICU IN
+        (
+        SELECT COD_ARTICU FROM ( SELECT TOP 10 COD_ARTICU, COUNT(COD_ARTICU) A FROM GVA53 WHERE FECHA_MOV >= GETDATE()-15 AND COD_ARTICU LIKE '[X]%' GROUP BY COD_ARTICU ORDER BY 2 DESC )A
+        )
+        ");
+        
+        foreach ($articulosNuevos as $articulo => $value) {
+            echo $value->COD_ARTICU.' ';
+            echo $value->DESCRIPCIO.' ';
+            echo $value->DESC_ADIC.'<br>';
+        }
+
+        $url = 'https://5ff39a8228c3980017b196e3.mockapi.io/v1/datos/';
+    
+        $client = new \GuzzleHttp\Client();
+
+        $request = $client->post($url, [
+            'form_params' => [
+                'nombre'=> 'gaston', 
+                'apellido'=> 'marcilio',
+                'email'=> 'hola@hola.com'
+            ]
+        ]);
+        
+        // return $articulosNuevos;
+            
+    }
+
+
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,39 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function () { return view('welcome'); });
+
+Route::get('/articulosNuevos', 'ProductsController@articulosNuevos')->name('articulosNuevos');
+
+
+
+Route::get('/httpclient/get/{id?}', function($id=null){
+    $url = 'https://5ff39a8228c3980017b196e3.mockapi.io/v1/datos/';
+    
+    $client = new \GuzzleHttp\Client();
+    $request = $client->get($url.$id);
+    $response = $request->getBody();
+
+    return $response;
+
+});
+
+
+Route::get('/httpclient/post', function(){
+    $url = 'https://5ff39a8228c3980017b196e3.mockapi.io/v1/datos/';
+    
+    $client = new \GuzzleHttp\Client();
+
+    $request = $client->post($url, [
+        'form_params' => [
+            'nombre'=> 'gaston', 
+            'apellido'=> 'marcilio',
+            'email'=> 'hola@hola.com'
+        ]
+    ]);
+
+    $response = $request->getBody();
+    // return $response;
+    return redirect('/httpclient/get/');
+    
 });
